42c0016c5878b0f55e645af3520c11d2eab8bd91
volvo64 <33491829+volvo64@users.noreply.github.com>
2018-03-13 15:12:46 -0700

    chapter 2

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+# This program says Hello and asks for my name
+
+print ('Hello world!')
+print ('What is your name') # ask for name
+myName= input()
+print ('It is good to meet you, ' + myName)
+print ('the length of your name is:')
+print(len(myName))
+print ('What is your age?') #ask for age
+myAge=input()
+print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
